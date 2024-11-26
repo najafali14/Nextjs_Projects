f2e97213667a66658b48481117f0bcbdff07fc9f
Najaf Ali <65591681+najafali14@users.noreply.github.com>
2024-11-26 18:51:29 +0500

    Create page.tsx

---
+++ nextjs-css-multipage/src/app/projects/page.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image"
+import hbg from "../../public/abg.png"
+export default function Projects(){
+
+  return (
+    <div>
+      <div className="main">
+        <div className="about">
+   <h1 className="name">Projects </h1>
+
+         <h1 className="intro"> <a href="https://nysquiz.com">NYS QUIZ</a></h1>
+
+               <h1 className="intro"> <a href="https://github.com/najafali14">Github </a></h1>
+           <h1 className="intro"> <a href="https://github.com/najafali14">Nextjs Projects</a></h1>
+           <h1 className="intro"> <a href="https://github.com/najafali14">GenAI Projects
+</a></h1>
+
+
+
+
+
+
+          </div>
+
+        <div className="img">
+      <Image src={hbg} width={"900"} height={"900"} alt="logo" className="himg"/>
+          </div>
+        </div>
+      <button className="p" style={{marginRight:"7px"}} ><a href="/projects">Projects</a></button>
+      <button className="ab" style={{marginRight:"7px"}} ><a href="/about">About</a></button>
+      <button className="c" style={{marginRight:"7px"}} ><a href="/contact">Contact</a></button>
+    </div>
+  )
+}
